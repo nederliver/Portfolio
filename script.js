00f8113f2512293b0ae3aef5cd9f0d756b99e73c
nederliver <minibeary@icloud.com>
2025-07-21 22:23:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const nonDraggableImages = document.querySelectorAll('.no-drag');
+    nonDraggableImages.forEach(img => img.setAttribute('draggable', false));
+
+    // Make the links on the "links" page open in a new tab
+    const linkItems = document.querySelectorAll('.link-item');
+    linkItems.forEach(item => {
+        item.addEventListener('click', () => {
+            window.open(item.dataset.url, "_blank");
+        });
+    });
+    const donate = document.querySelectorAll('.donate-title');
+    donate.forEach(item => {
+        item.addEventListener('click', () => {
+            window.open(item.dataset.url, "_blank");
+        });
+    });
+}); 
